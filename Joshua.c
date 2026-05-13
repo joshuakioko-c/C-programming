68ddc3ad1dee581ff1d6c0a02ad1e3b75d0cbd57
supervisor-josh <kiokojoshua@students.zetech.ac.ke>
2026-05-13 10:28:16 +0300

    Add files via upload

---
+++ Joshua.c
@@ -0,0 +1,20 @@
+//Program to display the academic units offered
+
+/*
+Description: Program to display academic unitts offered in zetech university
+Name: Joshua Kioko
+Admn NO: BCS-03-0067/2026
+Date: 13/05/2026
+Version 1
+*/
+#include <stdio.h>
+
+int main(){
+	printf("Units offered this semister\n");
+	printf("1. Introduction to programming\n");
+	printf("2. Data structures\n");
+	printf("3. Computer networks\n");
+	printf("4. Database systems\n");
+	printf("5. Software enginering\n");
+	return 0;
+}	
